081c86a386d4b89b096e38ed8ae2a51f5c698a91
Brendan Higgins <bhiggins033@gmail.com>
2018-12-05 16:57:03 -0500

    Remove solution code from phase-01

--- src/frontend/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import reducer from './reducer.js';
-
-// instantiate app's store with app's reducer
-const store = createStore(reducer);
-export default store;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { addCleaning, addShopping, addTodo, removeTodo } from './frontend/actions';
-import store from './frontend/store';
-
-// put store and actions on the window
-window.store = store;
-window.addCleaning = addCleaning;
-window.addShopping = addShopping;
-window.addTodo = addTodo;
-window.removeTodo = removeTodo
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
